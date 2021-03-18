7f853360b761a9a0677bf8aeeac51e40993788eb
Sellin <you@example.com>
2021-03-18 11:36:25 +0100

    db uri

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 9999;
 
 const serverURI = 'http://localhost:' + PORT
 const mongoURI = process.env.MONGO_URI;
+// 'mongodb+srv://JRL:rally@cluster0.bkhg7.mongodb.net/REST?retryWrites=true&w=majority'
 
 app.listen(PORT, () => console.log('Server running at:' + serverURI));
 
