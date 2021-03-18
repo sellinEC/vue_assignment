f654bd3f7b112dc176383a2a327cf637a89f4acc
Sellin <you@example.com>
2021-03-18 13:05:57 +0100

    output

---
+++ client/src/components/Product.vue
@@ -0,0 +1,16 @@
+<template>
+ <div class="product">
+   <h1>{{product.title}}</h1>
+   <h2>{{product.desc}}</h2>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Product'
+}
+</script>
+
+<style>
+
+</style>
